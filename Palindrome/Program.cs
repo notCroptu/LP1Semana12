4854df1471868a7257b691933d80eb3b11c57216
notCroptu <martinsmariana1972@gmail.com>
2024-05-30 17:28:16 +0100

    Ex1 Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                bool isPalindrome = IsPalindrome(arg);
+                Console.WriteLine($"{arg} -> {isPalindrome}");
+            }
+        }
+        static bool IsPalindrome(string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+
+            if (input.Length < 2) return true;
+            
+            bool CheckPalindrome(string input)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (s[i] != s[input.Length - i]) return false;
+                }
+                return true;
+            }
+
+            return CheckPalindrome(input);
+        }
+    }
+}
